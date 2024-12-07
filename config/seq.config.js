a934a19fff077783614eb6c3af04b1429f61aa55
anand <vermaanand278@gmail.com>
2024-12-07 14:51:04 +0530

    asfadsfas

--- config/seq.config.js
+++ config/seq.config.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
-  "zupeetercm_zptrrrrlhdjnew",// db name
-  "root",// username
+  "zupeetercm_zptrrrrlhdjnewtest",// db name
+  "zupeetercm_zptrrrrlhdjnewtest",// username
   "Sudheer@123", // password
   {
     dialect: "mysql",
-    host: "51.21.22.154",
+    host: "103.120.176.66",
     logging: false,
   }
 );
